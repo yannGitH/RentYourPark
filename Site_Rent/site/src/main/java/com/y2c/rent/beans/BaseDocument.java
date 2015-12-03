b4d89115088e83386b06d9e5aabf4ac599a98022
jpcasas <jpcasas@gmail.com>
2015-12-03 16:26:09 +0100

    Initial site Hippo CMS for build the site

---
+++ Site_Rent/site/src/main/java/com/y2c/rent/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.y2c.rent.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="Site_Rent:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
